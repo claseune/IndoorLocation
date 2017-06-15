17d76b67b107afdd796dedc66c2e44cb5a10a869
Ernesto Nava <netux3@gmail.com>
2017-06-15 18:51:43 -0500

    Primer agregado cuestionario

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import * as firebase from 'firebase';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,10 +13,26 @@ export class HomePage {
 
   info: any;
   record: FirebaseListObservable<any>;
+  private exampleFormData: FormGroup;
 
-  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, db: AngularFireDatabase, public FormBuilder: FormBuilder) {
       this.record = db.list('/usuarios')
+
+      this.exampleFormData = this.FormBuilder.group({
+        name: ['',Validators.required],
+        gender: ['',Validators.required],
+        color: ['',Validators.required]
+      })
+
   }
+
+save(){
+  if(this.exampleFormData.valid){
+    this.record.push(this.exampleFormData.value)
+  }else{
+    console.error('verifique su informacion')
+  }
+}
 
   ionViewDidLoad() {
     console.log(this.record)
